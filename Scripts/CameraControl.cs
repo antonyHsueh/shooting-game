437f429c18485b91b196e568c7e912412f43d3fd
林 <inifeng@whu.edu.cn>
2019-12-06 00:05:07 +0800

    Scripts

---
+++ Scripts/CameraControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform GameObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(GameObject.position.x, GameObject.position.y, -10f);
+    }
+}
---
+++ Scripts/playeractor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playeractor : MonoBehaviour
+{
+    public GameObject modle;
+    public playerinput pi;
+    public Rigidbody2D rigid2d;
+    public float walkspeed=5.0f;
+    public Animator anim;
+
+
+    void Awake()
+    {
+        pi = GetComponent<playerinput> ();
+        rigid2d = GetComponent<Rigidbody2D> ();
+        modle = this.gameObject.transform.GetChild(0).gameObject;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rigid2d.position += pi.dvec * walkspeed * Time.fixedDeltaTime;
+        
+    }
+}
